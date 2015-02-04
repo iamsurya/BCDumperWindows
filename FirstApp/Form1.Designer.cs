d703314d500ec683c5846ee2f7ade9008eecb727
iamsurya <s@iamsurya.com>
2015-02-04 18:57:59 -0500

    Initial Commit

---
+++ FirstApp/Form1.Designer.cs
@@ -0,0 +1,168 @@
+﻿namespace FirstApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.IO.Ports.SerialPort serialPort1;
+            this.lblStat = new System.Windows.Forms.Label();
+            this.lblTXPercent = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.lblFileName = new System.Windows.Forms.Label();
+            this.BtnDump = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.lblRX = new System.Windows.Forms.Label();
+            this.lblToRX = new System.Windows.Forms.Label();
+            this.lblETA = new System.Windows.Forms.Label();
+            serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.SuspendLayout();
+            // 
+            // lblStat
+            // 
+            this.lblStat.AutoSize = true;
+            this.lblStat.ForeColor = System.Drawing.Color.Red;
+            this.lblStat.Location = new System.Drawing.Point(46, 173);
+            this.lblStat.Name = "lblStat";
+            this.lblStat.Size = new System.Drawing.Size(37, 13);
+            this.lblStat.TabIndex = 0;
+            this.lblStat.Text = "Status";
+            // 
+            // lblTXPercent
+            // 
+            this.lblTXPercent.AutoSize = true;
+            this.lblTXPercent.Location = new System.Drawing.Point(46, 201);
+            this.lblTXPercent.Name = "lblTXPercent";
+            this.lblTXPercent.Size = new System.Drawing.Size(104, 13);
+            this.lblTXPercent.TabIndex = 1;
+            this.lblTXPercent.Text = "Transfer Percentage";
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // lblFileName
+            // 
+            this.lblFileName.AutoSize = true;
+            this.lblFileName.Location = new System.Drawing.Point(46, 229);
+            this.lblFileName.Name = "lblFileName";
+            this.lblFileName.Size = new System.Drawing.Size(51, 13);
+            this.lblFileName.TabIndex = 2;
+            this.lblFileName.Text = "FileName";
+            this.lblFileName.Click += new System.EventHandler(this.lblFileName_Click);
+            // 
+            // BtnDump
+            // 
+            this.BtnDump.Location = new System.Drawing.Point(88, 96);
+            this.BtnDump.Name = "BtnDump";
+            this.BtnDump.Size = new System.Drawing.Size(75, 23);
+            this.BtnDump.TabIndex = 3;
+            this.BtnDump.Text = "Dump Data";
+            this.BtnDump.UseVisualStyleBackColor = true;
+            this.BtnDump.Click += new System.EventHandler(this.BtnDump_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(250, 95);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(144, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Close File and Serial Port";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(88, 47);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(322, 23);
+            this.progressBar1.TabIndex = 5;
+            // 
+            // lblRX
+            // 
+            this.lblRX.AutoSize = true;
+            this.lblRX.Location = new System.Drawing.Point(294, 173);
+            this.lblRX.Name = "lblRX";
+            this.lblRX.Size = new System.Drawing.Size(51, 13);
+            this.lblRX.TabIndex = 6;
+            this.lblRX.Text = "RX Bytes";
+            // 
+            // lblToRX
+            // 
+            this.lblToRX.AutoSize = true;
+            this.lblToRX.Location = new System.Drawing.Point(294, 201);
+            this.lblToRX.Name = "lblToRX";
+            this.lblToRX.Size = new System.Drawing.Size(60, 13);
+            this.lblToRX.TabIndex = 7;
+            this.lblToRX.Text = "Total Bytes";
+            // 
+            // lblETA
+            // 
+            this.lblETA.AutoSize = true;
+            this.lblETA.Location = new System.Drawing.Point(297, 229);
+            this.lblETA.Name = "lblETA";
+            this.lblETA.Size = new System.Drawing.Size(83, 13);
+            this.lblETA.TabIndex = 8;
+            this.lblETA.Text = "Time Remaining";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(569, 295);
+            this.Controls.Add(this.lblETA);
+            this.Controls.Add(this.lblToRX);
+            this.Controls.Add(this.lblRX);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.BtnDump);
+            this.Controls.Add(this.lblFileName);
+            this.Controls.Add(this.lblTXPercent);
+            this.Controls.Add(this.lblStat);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblStat;
+        private System.Windows.Forms.Label lblTXPercent;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label lblFileName;
+        private System.Windows.Forms.Button BtnDump;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label lblRX;
+        private System.Windows.Forms.Label lblToRX;
+        private System.Windows.Forms.Label lblETA;
+    }
+}
+
---
+++ FirstApp/Form1.cs
@@ -0,0 +1,275 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.IO.Ports;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FirstApp
+{
+    public partial class Form1 : Form
+    {
+        uint TimerCtr = 0;
+        uint TotalTime = 0;
+        uint READINGSPERPAGE = 170;
+        uint READINGS = 6;
+        Thread t;
+        ManualResetEvent runThread = new ManualResetEvent(false);
+        
+        uint NumReceived = 0;
+        ushort NumToReceive = 0;
+        uint NumToRXInt = 0;
+        string DumpFileName;
+        DateTime DTRightNow;
+        TimeSpan ETALeft;
+        string SaveDefaultDirectory;
+        /* Create a Stream to write to File */
+        BinaryWriter b;
+        byte[] Buffer = new byte[4];
+
+        bool Command = false;
+
+        uint PBValue = 0;
+
+        /* Create a serial port */
+        SerialPort ComPort1 = new SerialPort("COM5", 115200);
+
+
+        private void ReceiveThread()
+        {
+            Debug.WriteLine("Debug Thread (Re)Started");
+            while (true)
+            {
+                runThread.WaitOne(Timeout.Infinite);
+
+                while (true)
+                {
+                    try
+                    {
+                        if(Command)
+                        { 
+                        // receive data 
+                            Buffer[2] = (byte)ComPort1.ReadByte();
+                            Buffer[3] = (byte)ComPort1.ReadByte();
+                            Buffer[0] = (byte)ComPort1.ReadByte();
+                            Buffer[1] = (byte)ComPort1.ReadByte();
+
+                            if((Buffer[2] != 'n') || (Buffer[3] != 'c'))
+                            {
+                                ComPort1.Close();
+                                b.Close();
+                                lblStat.Parent.Invoke((MethodInvoker)delegate { lblStat.Text = "\'nc\' NOT received. File and Stream Closed"; lblStat.ForeColor = Color.Red; });
+                                return;
+                            }
+                        NumToReceive = (ushort)(((ushort)Buffer[1] << 8) + (ushort)Buffer[0]);
+                        NumToRXInt = ((uint)NumToReceive * (READINGSPERPAGE) * READINGS);
+                        //lblToRX.Parent.Invoke((MethodInvoker)delegate { lblToRX.Text = NumToRXInt.ToString(); });
+                        
+
+                        Command = false;
+                        runThread.Reset();
+                        }
+                        else
+                        {
+                            b.Write((byte)ComPort1.ReadByte());
+
+                            TimerCtr = 0;
+                            NumReceived++;
+
+                            PBValue = (uint)(((double)NumReceived / (double)NumToRXInt) * (double)100);
+                            
+                            //progressBar1.Parent.Invoke((MethodInvoker)delegate{ progressBar1.Value = (int) PBValue; });
+
+                            //lblRX.Parent.Invoke((MethodInvoker)delegate
+                            //{
+                            //    lblRX.Text = NumReceived.ToString();
+
+                            //});
+
+
+                            if(NumReceived > (NumToRXInt-1) )
+                            {
+                                try
+                                {
+                                    //Debug.WriteLine(TotalTime.ToString());
+                                    ComPort1.Close();
+                                    b.Close();
+                                    lblStat.Parent.Invoke((MethodInvoker)delegate {
+                                                lblStat.Text = "Data Finished, File and Stream Closed";
+                                                lblStat.ForeColor = Color.Green; 
+                                    
+                                    });
+                                    
+                                    //lblStat.Parent.Invoke((MethodInvoker)delegate { lblETA.Text = TotalTime.ToString(); });
+                                    
+                                }
+                                catch (Exception ex)
+                                {
+                                    MessageBox.Show(ex.Message, "", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                                    return;
+                                }
+                            }
+
+                        }
+                        /* Store data */
+                        //                        this.Invoke(this.m_DelegateAddToList, new Object[] { "R: " + msg });
+                    }
+
+                    catch(Exception ex)
+                    {
+                        Debug.WriteLine("Caught " + ex.Message);
+                        runThread.Reset();
+                        break;
+                    }
+
+                    
+
+                }
+            }
+        }
+
+
+        public Form1()
+        {
+            InitializeComponent();
+
+
+            t = new Thread(ReceiveThread);
+            t.Start();
+            
+        }
+
+        
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            TimerCtr++;
+            TotalTime++;
+            lblRX.Text = NumReceived.ToString();
+            progressBar1.Value = (int)PBValue;
+            
+            lblTXPercent.Text = PBValue.ToString()+"%";
+            lblToRX.Text = NumToRXInt.ToString();
+            
+            ETALeft = new TimeSpan( ((NumToRXInt - NumReceived) / 14400)*TimeSpan.TicksPerSecond);
+            lblETA.Text = ETALeft.ToString();
+            
+            /* If NO data received */
+            if (TimerCtr>30 && ComPort1.IsOpen)
+            {
+                lblStat.Parent.Invoke((MethodInvoker)delegate { lblStat.Text = "RX error."; lblStat.ForeColor = Color.Red; });
+                ModifyProgressBarColor.SetState(progressBar1, 2); // 2 = red
+                try { 
+                ComPort1.Close();
+                
+                }
+                catch
+                {
+
+                }
+
+                b.Close();
+            }
+
+        }
+
+
+
+        private void BtnDump_Click(object sender, EventArgs e)
+        {
+
+            try
+                {
+                    ComPort1.Open();
+                    TimerCtr = 0;
+                    TotalTime = 0;
+                    timer1.Enabled = true;
+                    ComPort1.DiscardInBuffer();
+                    ETALeft = new TimeSpan(0);
+                }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+
+            /* Get the current Data and Time to use for filename */
+            DTRightNow = DateTime.Now;
+
+            DumpFileName = DTRightNow.Year.ToString() + "_" + DTRightNow.Month.ToString() + "_" + DTRightNow.Day.ToString() + "T" + DTRightNow.Hour.ToString() + "_" + DTRightNow.Minute.ToString() + "_" + DTRightNow.Second.ToString() + ".wrdt";
+
+            SaveDefaultDirectory = "C:\\Users\\Surya\\Dropbox\\Education\\BiteCounter\\WristData Dump\\NewAppDump\\";
+
+        
+            lblFileName.Text = DumpFileName;
+            /* Write to the file */
+
+
+            b = new BinaryWriter(File.Open(SaveDefaultDirectory + DumpFileName, FileMode.Create));
+            ModifyProgressBarColor.SetState(progressBar1, 1); // 1 = Green
+            
+            /* Open the Serial Port */
+            NumReceived = 0;
+            NumToReceive = 0;
+
+            /* The "Send Data" sd command */
+            ComPort1.Write("sd");
+            Command = true;
+            runThread.Set();
+            lblStat.Parent.Invoke((MethodInvoker)delegate { lblStat.Text = "Command Sent"; lblStat.ForeColor = Color.Green; });
+
+
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //b.Close();
+            try
+            {
+                
+                ComPort1.Close();
+                b.Close();
+                t.Abort();
+                t = new Thread(ReceiveThread);
+                t.Start();
+                lblStat.Text = "File and Stream Closed \n";
+                lblStat.ForeColor = Color.YellowGreen;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+        }
+
+        private void lblFileName_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start((SaveDefaultDirectory + DumpFileName));
+        }
+
+
+    }
+}
+
+
+/* Change Progress Bar Color */
+
+public static class ModifyProgressBarColor
+{
+    [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = false)]
+    static extern IntPtr SendMessage(IntPtr hWnd, uint Msg, IntPtr w, IntPtr l);
+    public static void SetState(this ProgressBar pBar, int state)
+    {
+        SendMessage(pBar.Handle, 1040, (IntPtr)state, IntPtr.Zero);
+    }
+}
